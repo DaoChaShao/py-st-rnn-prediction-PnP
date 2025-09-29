c88628df1010d16b58b6e7eaa46bf623e31cac41
DaoChaShao <27882272@qq.com>
2025-09-29 19:47:24 +0800

    feat: add layout configuration and sidebar navigation for application pages

--- utils/layout.py
+++ utils/layout.py
@@ -6,3 +6,69 @@
 # @File     :   layout.py
 # @Desc     :   
 
+from streamlit import set_page_config, Page, navigation
+
+
+def page_config() -> None:
+    """ Set the window
+    :return: None
+    """
+    set_page_config(
+        page_title="RNN Sequences Generation Prediction",
+        page_icon=":material/globe:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+
+def pages_setter() -> None:
+    """ Set the subpages on the sidebar
+    :return: None
+    """
+    pages: dict = {
+        "page": [
+            "subpages/home.py",
+            "subpages/preparation.py",
+            "subpages/analysis.py",
+            "subpages/train.py",
+            "subpages/test.py",
+            "subpages/realtime.py",
+            "subpages/about.py",
+        ],
+        "title": [
+            "Home",
+            "Data Preparation",
+            "Data Analysis",
+            "Model Training",
+            "Model Testing",
+            "Real-time Prediction",
+            "About",
+        ],
+        "icon": [
+            ":material/home:",
+            ":material/dataset:",
+            ":material/bar_chart:",
+            ":material/function:",
+            ":material/assignment:",
+            ":material/assessment:",
+            ":material/info:",
+        ],
+    }
+
+    structure: dict = {
+        "Introduction": [
+            Page(page=pages["page"][0], title=pages["title"][0], icon=pages["icon"][0]),
+        ],
+        "Core Functions": [
+            Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
+            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
+            Page(page=pages["page"][4], title=pages["title"][4], icon=pages["icon"][4]),
+            Page(page=pages["page"][5], title=pages["title"][5], icon=pages["icon"][5]),
+        ],
+        "Information": [
+            Page(page=pages["page"][6], title=pages["title"][6], icon=pages["icon"][6]),
+        ],
+    }
+    pg = navigation(structure, position="sidebar", expanded=True)
+    pg.run()
