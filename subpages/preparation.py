8c3f8b9753591635d677f8b4b5fc42b453d86a1b
DaoChaShao <27882272@qq.com>
2025-09-29 20:07:26 +0800

    feat: implement text data preparation interface with loading and clearing functionality

--- subpages/preparation.py
+++ subpages/preparation.py
@@ -6,4 +6,36 @@
 # @File     :   preparation.py
 # @Desc     :   
 
+from streamlit import (empty, sidebar, subheader, session_state,
+                       button, spinner, rerun)
 
+from utils.config import TEXT_FILE_PATH
+from utils.helper import Timer, article_txt_reader
+
+empty_messages: empty = empty()
+empty_articles: empty = empty()
+
+pre_sessions: list[str] = ["raw", "pTimer"]
+for session in pre_sessions:
+    session_state.setdefault(session, None)
+
+with sidebar:
+    subheader("📚 Text Preparation")
+
+    if session_state["raw"] is None:
+        empty_messages.error("Click the button below to start preparing the text data.")
+
+        if button("Load the Raw Text Data", type="primary", width="stretch"):
+            with spinner("Preparing the text data...", show_time=True, width="stretch"):
+                with Timer("Load the Raw Text Data") as session_state["pTimer"]:
+                    session_state["raw"]: str = article_txt_reader(TEXT_FILE_PATH)
+            rerun()
+    else:
+        empty_messages.success(f"{session_state["pTimer"]} Text data is prepared. You can proceed to the next step.")
+
+        empty_articles.markdown(session_state["raw"])
+
+        if button("Clear the Text Data", type="secondary", width="stretch"):
+            for session in pre_sessions:
+                session_state[session] = None
+            rerun()
