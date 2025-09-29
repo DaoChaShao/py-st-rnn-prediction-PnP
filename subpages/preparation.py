d71852de82c16a67b3528e069e7d801bb8a3bafa
DaoChaShao <27882272@qq.com>
2025-09-30 01:09:11 +0800

    refactor: update timer session names for consistency in text data preparation

--- subpages/preparation.py
+++ subpages/preparation.py
@@ -10,12 +10,12 @@ from streamlit import (empty, sidebar, subheader, session_state,
                        button, spinner, rerun)
 
 from utils.config import TEXT_FILE_PATH
-from utils.helper import Timer, article_txt_reader
+from utils.helper import Timer, txt_reader
 
 empty_messages: empty = empty()
 empty_articles: empty = empty()
 
-pre_sessions: list[str] = ["raw", "pTimer"]
+pre_sessions: list[str] = ["raw", "eTimer"]
 for session in pre_sessions:
     session_state.setdefault(session, None)
 
@@ -27,11 +27,11 @@ with sidebar:
 
         if button("Load the Raw Text Data", type="primary", width="stretch"):
             with spinner("Preparing the text data...", show_time=True, width="stretch"):
-                with Timer("Load the Raw Text Data") as session_state["pTimer"]:
-                    session_state["raw"]: str = article_txt_reader(TEXT_FILE_PATH)
+                with Timer("Load the Raw Text Data") as session_state["eTimer"]:
+                    session_state["raw"]: str = txt_reader(TEXT_FILE_PATH)
             rerun()
     else:
-        empty_messages.success(f"{session_state["pTimer"]} Text data is prepared. You can proceed to the next step.")
+        empty_messages.success(f"{session_state["eTimer"]} Text data is prepared. You can proceed to the next step.")
 
         empty_articles.markdown(session_state["raw"])
 
