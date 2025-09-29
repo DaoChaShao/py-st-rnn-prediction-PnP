2592a2eff8494aee2ca0eff115cd653d55a4f011
DaoChaShao <27882272@qq.com>
2025-09-29 19:47:20 +0800

    feat: update home.py with introductory title and informational messages

--- subpages/home.py
+++ subpages/home.py
@@ -6,4 +6,11 @@
 # @File     :   home.py
 # @Desc     :   
 
+from streamlit import title, expander, caption, empty
 
+empty_messages = empty()
+empty_messages.info("Please check the details at the different pages of core functions.")
+
+title("Recurrent Neural Network (RNN) - Sequences Generation Prediction")
+with expander("**INTRODUCTION**", expanded=True):
+    caption("+")
